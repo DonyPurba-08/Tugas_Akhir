dfae27be99ed5fc3a24495a8baaa6d6f17885122
SKY_TRACK <doniandripurba@gmail.com>
2023-07-13 23:20:24 +0800

    Tugas_Akhir

---
+++ homepage.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/desain.css">
+	<title>TUGAS AKHIR</title>
+</head>
+<body>
+		<header>	
+			<div id= "medsos">
+				<a href="https://twitter.com" id = "twitter" target="_blank">twitter.com</a>
+				<a href="https://instagram.com" id = "instagram" target="_blank">ig.com</a>
+				<a href="https://facebook.com" id = "facebook" target="_blank">fb.com</a>
+				<a href="https://web.whatsapp.com/.com" id = "whatsapp" target="_blank">wa.com</a>
+			</div>
+
+			<div id = "logo"><img src="img/LOGO_PNKDG.png"></div>
+				<h1 id = "instansi">PENGADILAN NEGERI KANDANGAN</h1>
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="https://pn-kandangan.go.id/" target="_blank" rel="nofollow">Tentang</a></li>
+					<li><a href="https://pn-kandangan.go.id/index.php/tentang-pengadilan/profil-pengadilan/pengantar-ketua-pengadilan" target="_blank" rel="nofollow">Profil</a></li>
+					<li><a href="https://pn-kandangan.go.id/index.php/layanan-publik/ptsp/standard-pelayanan" target="_blank" rel="nofollow">Layanan</a></li>
+					<li><a href="https://pn-kandangan.go.id/index.php/layanan-publik/permohonan-informasi" target="_blank" rel="nofollow">Kontak</a></li>
+				</ul>
+		</header>
+
+		<main>
+				<div class="wrapper">
+					<div class="slides">
+						<span id="slide-1"></span>
+						<span id="slide-2"></span>
+						<span id="slide-3"></span>
+						<span id="slide-4"></span>
+						<div class = "images">
+							<img src="img/pnkanda.jpg">
+							<img src="img/parkirmotor.jpg" >
+							<img src="img/parkirumum.jpg">
+							<img src="img/parkirmobil.jpg">
+						</div>
+						<div class="navigasi">
+							<a href="#slide-1">1</a>
+							<a href="#slide-2">2</a>
+							<a href="#slide-3">3</a>
+							<a href="#slide-4">4</a>
+						</div>
+					</div>
+				</div>
+				<div id="gedung">Foto: Gedung Pengadilan Negeri Kandangan</div>
+				
+			<article>
+					<h1 id = "judul">Sejarah Lembaga Pengadilan Negeri Kandangan</h1>						
+					<h1 id ="penulis">ditulis oleh <a href="https://pn-kandangan.go.id/index.php/tentang-pengadilan/profil-pengadilan/profil-hakim-pegawai-pn/profil-staf-kesekretariatan/profil-sub-bagian-perencanaan-ti-dan-pelaporan" target="_blank" rel="nofollow">Dony A. R. Purba</a> pada 06 Juli 2023</h1>
+
+					<p>Pada zaman Pemerintahan Hindia Belanda untuk Daerah Hulu Sungai sudah ada Landrad, dan pada saat itu daerah Hulu Sungai pemerintahan dipegang oleh seorang bangsa Belanda yang berpangkat Assisten Residen disamping dikenal jabatan yang dipegang oleh orang asal pribumi dengan sebutan Kyai Besar. Sebenarnya jabatan Kyai Besar ini adalah anugerah dari Ratu Belanda yang diberikan kepada salah seorang dari 5 (lima) Kyai Kepala dijadikan oleh Ratu Belanda menjadi Kyai Besar yang berkedudukan di Kandangan. Di daerah Hulu Sungai terdapat Kyai Kepala Tapin, Kandangan, Amuntai, Barabai, dan Tabalong yang masing-masing mempunyai wilayah yang sama dengan wilayah Distrik yang dikepalai oleh seorang kontrolir.</p>
+
+					<p>Sedang daerah hukum wilayah Peradilan di Hulu Sungai pada saat itu selain meliputi daerah yang saat sekarang disebut dengan sebutan Benua Lima yang meliputi Kabupaten Hulu Sungai Utara, Kabupaten Hulu Sungai Selatan, Kabupaten Tapin, Kabupaten Tabalong juga termasuk Tamiyang Layang dan Ampah, yang kemudian terbentuknya Provinsi Kalimantan Tengah kedua daerah ini masuk menjadi wilayah Propinsi Kalimantan Tengah. Pada zaman pemerintahan bala tentara Jepang (saat masa transisi yaitu setelah Jepang masuk kota Kandangan sampai waktu lima bulan dihitung dengan masa pemerintahan Hindia Belanda di Kandangan yaitu Assisten Residen menyerah pada bala tentara Jepang diatas jembatan Kali Amandit, pada  saat itu Assisten Residen di Kandangan bernama Bykiek, beserta dua orang kantrolirnya mati dibunuh oleh bala tentara Jepang yang baru saja masuk dan berada diatas jembatan Antaludin yang kedapatan sudah dirusak dan diputus. Selanjutnya bala tentara Jepang mulai melaksanakan Peradilan di Hulu Sungai Selatan dengan susunan sebagai berikut: <br>
+					- Hakim dari bala tentara Jepang. <br>
+					- anitera dari orang pribumi. <br>
+					- Jaksa dari orang pribumi.</p>
+
+					<p>Jalannya Peradilan pada masa transisi tadi adalah jaksa berhak mengemukakan tuduhan dan tuntutan terhadap si tertuduh dan setelah keputusam dijatuhkan oleh Hakim bala tentara Jepang, si terhukum (selama masa transisi ini) tidak pernah diberi hak untuk bisa banding. Setelah masa transisi diatas berlaku yakni sekitar mulai tahun 1943 Peradilan di Hulu Sungai dikenal dimasa zaman Jepang dengan sebutan Tjihao Hoin, susunan Peradilannya adalah Hakim orang pribumi bernama Mastayana, Panitera bernama Sunaryo lalu diganti oleh Anang Miskin, ada anggota dari persidangan selaku Penasehat (bukan hakim) dan seorang petugas Agama (Mufti) selaku Juru Sumpah.</p>
+
+						
+					<p>Setelah Jepang tidak berkuasa, kembali peradilan di Hulu Sungai dipegang pemerintahan Belanda yang pada waktu itu kembali bersistem Peradilan Hindia Belanda dan untuk Hulu Sungai ada landgerecht dengan landgerechtnya berjalan kurang lebih 2 tahun setelah 1945) yaitu setelah habis masa tugas Mr. S.T.I. Tan maka tahun 1947 diganti oleh seorang Indo Belanda (Maluku) bernama Mr. W.A.Muller di Hulu Sungai habis maka diganti oleh Sdr. Baseri yang dikenal dengan sebutan sebagai Hakim Peradilan Hulu Sungai di Kandangan. Pada uraian tentang mengenal daerah Hulu Sungai Selatan yakni dengan adanya SK. Gubernur Kalimantan No. 154/PB/92/04 tanggal 3 Agustus 1950 yakni ditetapkannya susunan wilayah dan jabatan pemerintahan dalam propinsi Kalimantan Selatan antara lain Afdelling Hulu Sungai menjadi Kabupaten Hulu Sungai, maka timbul desakan-desakan dari rakyat dan partai-partai politik serta organisasi massa, maka oleh Gubernur Kalimantan Selatan dikeluarkan Surat Keputusan tanggal 14 Agustus 1950 No. 186/OPB/92/14 yang menetapkan Peraturan sementara Pembentukan daerah-daerah otonom di Kalimantan.</p>
+						
+					<p>Selanjutnya dengan adanya Surat Keputusan Menteri Dalam Negeri tanggal 16 Nopember 1951 No. Perm.20/1/47 maka Kabupaten Hulu Sungai Selatan di bagi menjadi dua daerah yaitu Kabupaten Hulu Sungai Selatan dengan ibukota Kandangan dan Kabupaten Hulu Sungai Utara dengan ibukotanya Amuntai. Serta berdasarkan Undang-Undang No.27 tahun 1959 yang diundangkan pada tanggal 4 Juni 1959 Kabupaten Kandangan sejak tanggal 26 Juni 1959 dibagi menjadi dua daerah otonom tingkat II, yaitu Kabupaten Dati II Hulu Sungai Selatan dengan ibukotanya Kandangan dan Kabupaten Dati II Hulu Sungai Tengah dengan ibukotanya Barabai. Selanjutnya pada tahun 1965 Kabupaten Hulu Sungai Selatan masih dibagi dua lagi yaitu Kabupaten Hulu Sungai Selatan dengan ibukotanya Kandangan dan Kebupaten Tapin dengan ibukotanya Rantau. Dengan telah terbentuk dan disahkannya 5 (lima) daerah otonom yakni Hulu Sungai Utara, Hulu Sungai Tengah, Hulu Sungai Selatan, Tabalong, dan Tapin, sebagaimana cita daripada Peradilan di Negara Kesatuan Republik Indonesia yakni bahwa setiap Daerah Tingkat II (Kabupaten dan Kotamadya) dikehendakkan berdiri/mempunyai Pengadilan Negeri.</p>
+
+					<p>Demikianlah sampai dengan saat ini didaerah Hulu Sungai dengan lima Daerah Kabupaten, mempunyai Pengadilan Negeri, yaitu: <br>
+						1. Pengadilan Negeri Rantau. <br>
+						2. Pengadilan Negeri Kandangan. <br>
+						3. Pengadilan Negeri Barabai. <br>
+						4. Pengadilan Negeri Amuntai. <br>
+						5. Pengadilan Negeri Tanjung.</p>
+			</article>
+				
+			<aside>
+					<h2>Tentang Penulis :</h2>
+					<img src="img/Dony.jpg">
+					<table border="1">
+						<tr>
+							<th>NAMA</th>
+							<td>DONY ANDRI RODESE PURBA, A.Md.</td>
+						</tr>
+						<tr>
+							<th>NIP</th>
+							<td>199--------------------------</td>
+							</tr>
+						<tr>
+							<th>JABATAN</th>
+							<td>Pengelola Sistem Dan Jaringan, Sub Bagian Perencanaan Teknologi Informasi, Dan Pelaporan Pengadilan Negeri Kandangan</td>
+						</tr>
+
+						<tr>
+							<th>PANGKAT/GOLONGAN</th>
+							<td>--------</td>
+						</tr>
+						<tr>
+							<th>TEMPAT TANGGAL LAHIR</th>
+							<td>Medan,08 Agustus 1994</td>
+						</tr>
+						<tr>
+							<th>JENIS KELAMIN</th>
+							<td>Pria</td>
+						</tr>
+						<tr>
+							<th>AGAMA</th>
+							<td>Kristen Protestan</td>
+						</tr>
+						<tr>
+							<th>PENDIDIKAN</th>
+							<td>- D-III TEKNIK INFORMATIKA USU Medan (2015)<br>
+								  -  SMAN 5 MEDAN (2012) <br>
+								 -  SMP KATOLIK TRISAKTI-MEDAN (2009) <br>  
+								-  SD ST ANTONIUS V MEDAN (2006)</td>
+						</tr>
+						<tr>
+							<th>ALAMAT RUMAH</th>
+							<td>Jalan Parindra, Gang Syahdan No 1, Kandangan Kota, Kab Hulu Sungai Selatan, Kalimantan Selatan</td>
+						</tr>
+					</table>
+			</aside>
+		</main>
+
+		<footer><p id="copy">Copyright &copy; 2023 Dony A. R. Purba</p></footer>
+			
+		
+</body>
+</html>
